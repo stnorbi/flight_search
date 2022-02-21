19077834df2e2b0f724afb4ef24cc1e37b0978ed
Stréling Norbert <streling.norbert@gmail.com>
2022-02-21 08:05:32 +0100

    modify argument parser

--- solution.py
+++ solution.py
@@ -1,8 +1,10 @@
 import sys, getopt
+import argparse
+from xmlrpc.client import boolean
 from modules import flights
 
 
-def main(argv):
+def main(argparse):
     
     '''
     It is dedicated to trigger flight search and that before receive mandatory and optional paramaters from terminal.
@@ -19,37 +21,29 @@ def main(argv):
     -r (--return): boolean value to flag search return flights as well
     
     '''
+    parser = argparse.ArgumentParser(description='Process input file path, code of airports and.'
+                                     ,epilog="If you want to take bag(s) or search for return flights please add parameters accordingly.")
     
-    bag=None
-    returns=None
-
-    try:
-      opts, args = getopt.getopt(argv,"b::r",["bags=","return"])
-    except getopt.GetoptError:
-      print('One of the arguments is invalid!')
-      sys.exit(2)
-    for opt, arg in opts:
-        if opt in ('--bags'):
-            bag=arg
-        if opt in ('--return'):
-            returns=eval('True')    
-    
-    if bag!=None and returns==None:
-        flights.search(sys.argv[1],sys.argv[2],sys.argv[3],bags=bag)
-    elif bag==None and returns!=None:
-        flights.search(sys.argv[1],sys.argv[2],sys.argv[3],returns=returns)
-    elif bag!=None and returns!=None:
-        flights.search(sys.argv[1],sys.argv[2],sys.argv[3],bags=bag,returns=returns)
-    else:
-        flights.search(sys.argv[1],sys.argv[2],sys.argv[3])
-        
+    parser.add_argument('filepath',metavar='datasource',type=str,nargs=1,
+                        help='File containing data set of flights.')
+    parser.add_argument('airport', metavar='airports', type=str, nargs=2,
+                    help='Please add airport codes (Origin and Destination airports as well)')
+    parser.add_argument('--bags', metavar='b', type=int, default=0,
+                    help='Indicates number of bags you want to take')
+    parser.add_argument('--return',type=bool,default=False,const="default",nargs='?',
+                        help="Indicate you want to search for return flights. Deafult is NO")
+    args = parser.parse_args()
+    print(args)
+    print(args.airport[0])
+    flights.search(args.filepath[0],args.airport[0],args.airport[1],bags=args.bags,returns=False)
         
 if __name__=="__main__":
     '''
     Call of main function to launch flight search.
     '''
+
     try:
-        main(sys.argv[4:])       
+        main(argparse)       
     except Exception as error:
         print(error)
         print('Flight research has been stoped for one of the reasons mentioned above.')   
