568a03f0cca011a87155609477b22f4e815c2aef
Stréling Norbert <streling.norbert@gmail.com>
2022-02-23 21:24:32 +0100

    carry on

--- test3.py
+++ test3.py
@@ -1,23 +1,43 @@
 import csv
 from collections import defaultdict
 import json
+import os
 
+os.system('clear')
 
 # add nodes
 def addNode(graph, nodeToAdd):
     if nodeToAdd not in graph:
-        graph[nodeToAdd]=set()
+        graph[nodeToAdd]=[]
 
     return graph
 
 # add connections:
 def addConnection(graph, origin, destination):
-    print(origin,destination)
+    #print(origin,destination)
     addNode(graph, origin)
     addNode(graph, destination)
+    if str(destination) not in graph[origin]:
+      graph[origin].append(str(destination))
+    
 
-    graph[origin].add(str(destination))
+    return graph
 
+# Add an edge between vertex v1 and v2 with edge weight e
+def add_edge(graph,v1, v2, e1):
+  # global graph
+  # Check if vertex v1 is a valid vertex
+  if v1 not in graph:
+    print("Vertex ", v1, " does not exist.")
+  # Check if vertex v2 is a valid vertex
+  elif v2 not in graph:
+    print("Vertex ", v2, " does not exist.")
+  else:
+    # Since this code is not restricted to a directed or 
+    # an undirected graph, an edge between v1 v2 does not
+    # imply that an edge exists between v2 and v1
+    temp = [v2, e1]
+    graph[v1].append(temp)
     return graph
 
 
@@ -60,15 +80,16 @@ def find_shortest_path(graph, start, end, path=[]):
 
 
 # add nodes
-graph=defaultdict(set)
+graph=defaultdict(list)
 filepath='example/example0.csv'
 with open(filepath) as csvFile:
     csvReader=csv.DictReader(csvFile)
     for row in csvReader:
         #graph=addNode(graph,row['origin'])
         graph=addConnection(graph, row['origin'],row['destination'])
+        #graph=add_edge(graph,row['origin'],row['destination'],row)
 
-print("Nodes:", graph)
+#print("Nodes:", graph)
 
 #add connection
 with open(filepath) as csvFile:
@@ -84,18 +105,64 @@ data=find_all_paths(graph, 'WIW', 'ECV')
 
 # shortdata=find_shortest_path(graph, 'WIW', 'ECV')
 # print(shortdata)
-for i in data:
-    print(i)
+# for i in data:
+#     print(i)
 
-list=[]
-with open(filepath) as csvFile:
-    csvReader=csv.DictReader(csvFile)
-    for row in csvReader:
-        for i in data:
-            print(i)
-            for j in range(0,len(i)-1):
-                if j==0 and row['origin']==i[j] and row['destination']==i[j+1]:
-                    print(row)
-                if j>=0 and row['origin']==i[j] and row['destination']==i[j+1]:
-                    if row['flight_no'] not in list:
-                        print(row)
+# list=[]
+# with open(filepath) as csvFile:
+#     csvReader=csv.DictReader(csvFile)
+#     for row in csvReader:
+#         for i in data:
+#             print(i)
+#             for j in range(0,len(i)-1):
+#                 if j==0 and row['origin']==i[j] and row['destination']==i[j+1]:
+#                     print(row)
+#                 if j>=0 and row['origin']==i[j] and row['destination']==i[j+1]:
+#                     if row['flight_no'] not in list:
+#                         print(row)
+
+
+class grapher:
+   def __init__(self,gdict=None):
+      if gdict is None:
+         gdict = {}
+      self.gdict = gdict
+   def edges(self):
+      return self.findedges()
+# Add the new edge
+   def AddEdge(self, edge):
+      edge = set(edge)
+      (vrtx1, vrtx2) = tuple(edge)
+      if vrtx1 in self.gdict:
+         self.gdict[vrtx1].append(vrtx2)
+      else:
+         self.gdict[vrtx1] = [vrtx2]
+# List the edge names
+   def findedges(self):
+      edgename = []
+      for vrtx in self.gdict:
+        for nxtvrtx in self.gdict[vrtx]:
+            if {nxtvrtx, vrtx} not in edgename:
+               edgename.append({vrtx, nxtvrtx})
+        return edgename
+
+# def generate_edges(graph):
+#   edges = []
+
+#   # for each node in graph
+#   for node in graph:
+#     print(node)
+#       # for each neighbour node of a single node
+#     for neighbour in graph[node]:
+#         #print(neighbour)
+#         # if edge exists then append
+#         edges.append((node, neighbour))
+#   return edges
+    
+
+gr=grapher(graph)
+edges=gr.findedges()
+print(edges)
+for i in edges:
+  print(add_edge(graph,'WIW','ECV',i))
+  
